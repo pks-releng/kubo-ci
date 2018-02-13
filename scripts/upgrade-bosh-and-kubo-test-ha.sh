257adf3cfda96ddaf224782e6252173aea335c51
Akshay Mankar <amankar@pivotal.io>
2018-02-13 10:59:00 +0000

    Copy kubeconfig from gcs resource before test
    [#155061434]

--- scripts/upgrade-bosh-and-kubo-test-ha.sh
+++ scripts/upgrade-bosh-and-kubo-test-ha.sh
@@ -30,6 +30,8 @@ copy_state_and_creds() {
 if [ -z ${LOCAL_DEV+x} ] || [ "$LOCAL_DEV" != "1" ]; then
   copy_state_and_creds
 fi
+
+set_kubeconfig
 run_upgrade_test update_bosh "$HA_MIN_SERVICE_AVAILABILITY" "bosh"
 upload_new_releases
 run_upgrade_test update_kubo "$HA_MIN_SERVICE_AVAILABILITY" "kubo"
--- scripts/upgrade-stemcell-test-ha.sh
+++ scripts/upgrade-stemcell-test-ha.sh
@@ -28,11 +28,6 @@ update_stemcell() {
   ${DIR}/deploy-k8s-instance.sh
 }
 
-set_kubeconfig() {
-  mkdir -p ~/.kube
-  cp gcs-kubeconfig/config ~/.kube/config
-}
-
 set_kubeconfig
 upload_new_releases
 run_upgrade_test update_stemcell "$HA_MIN_SERVICE_AVAILABILITY"
